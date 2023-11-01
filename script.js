9e3e2cb05a4b8c055eccda076cb059f995af2b99
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-01 19:15:22 +0000

    Update JS, CSS and add download CV

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// JavaScript-Code, um den "Nach oben"-Button anzuzeigen und zum Header zu scrollen
+// if you scrolled down from top to bottom 1200px, then the UP Button appears in the footer and you can use the Event Listener below.
 document.addEventListener("scroll", function() {
-    if (window.scrollY > 200) {
+    if (window.scrollY > 1200) {
         document.querySelector(".up-button").style.display = "block";
     } else {
         document.querySelector(".up-button").style.display = "none";
     }
 });
-
-// Scroll-Funktion zum Header
+// Click on the UP Button and you get up to the header, but smooth!
 document.querySelector(".up-button").addEventListener("click", function() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
