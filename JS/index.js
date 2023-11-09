34c8e07e0bf2b4aeab7919d2ed669fe02e445bf2
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-09 20:06:56 +0000

    fetch api finally works

--- JS/index.js
+++ JS/index.js
@@ -13,7 +13,17 @@ document.querySelector(".up-button").addEventListener("click", function() {
 
 // bad jokes API mit fetch soll hier erscheinen!
 
-
-
+async function fetchJoke() {
+  fetch('https://example-apis.vercel.app/api/bad-jokes/random')
+    .then(response => response.json())
+    .then(data => {
+      // Hier fügst du den Witz in das HTML-Dokument ein
+      document.getElementById('bad-jokes').innerHTML = data.joke;
+    })
+    .catch(error => {
+      console.error('Error fetching joke:', error);
+    });
+}
+fetchJoke();
 
 
